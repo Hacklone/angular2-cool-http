a8c5ed914f65a5d3605b01578287a433f7898fef
Barna Tóth <toakak@gmail.com>
2016-05-29 12:30:45 +0200

    Fix exporting

--- cool-http.ts
+++ cool-http.ts
@@ -1,6 +1,7 @@
 import { provide } from '@angular/core';
 
 import { CoolHttp } from './src/cool-http.service'
+export { CoolHttp } from './src/cool-http.service'
 
 export { HttpHeader } from './src/http-header.model'
 export { IRequestInterceptor } from './src/request-interceptor.interface'
