6db383d7ad15355300163b36a8c1b98fce7bafa4
Hacklone toakak@gmail.com <Barna.Toth@logmein.com>
2016-09-16 13:23:36 +0200

    Remove deprecated calls

--- cool-http.ts
+++ cool-http.ts
@@ -1,17 +1,12 @@
-import { NgModule, provide } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
-import { CoolHttp } from './src/cool-http.service'
-export { CoolHttp } from './src/cool-http.service'
+import { CoolHttp } from './src/cool-http.service';
+export { CoolHttp } from './src/cool-http.service';
 
-export { HttpHeader } from './src/http-header.model'
-export { IRequestInterceptor } from './src/request-interceptor.interface'
-export { IResponseInterceptor } from './src/response-interceptor.interface'
-
-/* @deprecated */
-export const COOL_HTTP_PROVIDERS: any[] = [
-    CoolHttp
-];
+export { HttpHeader } from './src/http-header.model';
+export { IRequestInterceptor } from './src/request-interceptor.interface';
+export { IResponseInterceptor } from './src/response-interceptor.interface';
 
 @NgModule({
     exports: [CoolHttp],
